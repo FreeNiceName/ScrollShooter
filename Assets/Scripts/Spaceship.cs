7ca8ded285f848406f9d46a2aeabc799403d7de0
d.haidylo <legendarnik98@gmail.com>
2021-09-16 13:59:04 +0300

    Add shield powerup

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -3,35 +3,51 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
-    private float _buffDuration = 5;
-    private bool _hasInvulerability = false;
+    [SerializeField] private float _shieldDuration = 5;
+
+    private GameObject _shield;
+    private bool _hasShield = false;
+
+    private void Start()
+    {
+        _shield = transform.Find("Shield").gameObject;
+        _shield?.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Powerup"))
         {
-            StartCoroutine(BuffCoroutine());
+            StartCoroutine(ShieldCoroutine());
             Destroy(other.gameObject);
         }
         else if (other.CompareTag("Projectile"))
         {
             if (other.gameObject.name.Contains("Enemy"))
             {
-                GameManager.Instance.Health -= other.GetComponent<ProjectileController>().Damage;
+                TakeDamage(other.GetComponent<ProjectileController>().Damage);
                 Destroy(other.gameObject);
             }
         }
         else if (other.CompareTag("Enemy"))
         {
-            GameManager.Instance.Health -= other.GetComponent<Enemy>().CollisionDamage;
+            TakeDamage(other.GetComponent<Enemy>().CollisionDamage);
             Destroy(other.transform.parent.gameObject);
         }
     }
 
-    private IEnumerator BuffCoroutine()
+    private IEnumerator ShieldCoroutine()
     {
-        _hasInvulerability = true;
-        yield return new WaitForSeconds(_buffDuration);
-        _hasInvulerability = false;
+        _hasShield = true;
+        _shield?.SetActive(true);
+        yield return new WaitForSeconds(_shieldDuration);
+        _hasShield = false;
+        _shield?.SetActive(false);
+    }
+
+    private void TakeDamage(int damage)
+    {
+        if(!_hasShield)
+            GameManager.Instance.Health -= damage;
     }
 }
